d9dbd2e35e32da172f4b054fd4d1bf261bc47fe9
jserme <dev.hubo@gmail.com>
2013-07-10 11:28:23 +0800

    代码格式调整

--- add.js
+++ add.js
@@ -7,38 +7,37 @@ var path = require('path');
 var keyArry = ['name', 'description', 'url', 'tags'];
 
 var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 var i = 0;
 var rst = {};
 
-const DATAPATH = './data';
+var DATAPATH = './data';
 
 function init() {
-	rl.question("site  " + keyArry[i] + ' is:', function(answer) {
-		rst[keyArry[i]] = answer;
-		i++;
+    rl.question("site  " + keyArry[i] + ' is:', function(answer) {
+        rst[keyArry[i]] = answer;
+        i++;
 
-		if (i == keyArry.length) {
-			fs.writeFile(path.join(DATAPATH, rst.name), JSON.stringify({
-				description: rst.description,
-				tags: rst.tags,
-				url: rst.url
-			}), function(err) {
-				if (err) {
-					throw err
-                } 
-			});
+        if (i == keyArry.length) {
+            fs.writeFile(path.join(DATAPATH, rst.name), JSON.stringify({
+                description: rst.description,
+                tags: rst.tags,
+                url: rst.url
+            }), function(err) {
+                if (err) {
+                    throw err;
+                }
+            });
 
-			i = 0;
-			rst = {};
-		}
+            i = 0;
+            rst = {};
+        }
 
-		init();
-	});
+        init();
+    });
 }
 
 init();
-
--- build.js
+++ build.js
@@ -11,73 +11,74 @@ data.tags = {};
 
 var fileCount;
 
-const DATAFOLDER = "./data";
-const TEMPLATEFILE = "./template.jade";
-const DSTFILE = "./index.html";
+var DATAFOLDER = "./data";
+var TEMPLATEFILE = "./template.jade";
+var DSTFILE = "./index.html";
 
 function prePaddingNum(num, count) {
-	var baseNum = Math.pow(10, count - 1);
-	if (parseInt(num, 10) < baseNum) {
-		return (baseNum + '').slice(1) + '' + num;
-	} else {
-		return num;
-	}
+    var baseNum = Math.pow(10, count - 1);
+    if (parseInt(num, 10) < baseNum) {
+        return (baseNum + '').slice(1) + '' + num;
+    } else {
+        return num;
+    }
 }
 
 function getCurDate() {
-	var d = new Date();
-	return d.getFullYear() + '-' + prePaddingNum(d.getMonth() + 1, 2) + '-' + prePaddingNum(d.getDate(), 2) + ' ' + prePaddingNum(d.getHours(), 2) + ':' + prePaddingNum(d.getMinutes(), 2) + ':' + prePaddingNum(d.getSeconds(), 2);
+    var d = new Date();
+    return d.getFullYear() + '-' + prePaddingNum(d.getMonth() + 1, 2) + '-' + prePaddingNum(d.getDate(), 2) + ' ' + prePaddingNum(d.getHours(), 2) + ':' + prePaddingNum(d.getMinutes(), 2) + ':' + prePaddingNum(d.getSeconds(), 2);
 }
 
 function parseTmpl() {
-	fs.readFile(TEMPLATEFILE, 'utf8', function(err, tmpl) {
-		//var fn = jade.compile(tmpl, {pretty : true});
-		var fn = jade.compile(tmpl, {
-			pretty: true
-		});
+    fs.readFile(TEMPLATEFILE, 'utf8', function(err, tmpl) {
+        //var fn = jade.compile(tmpl, {pretty : true});
+        var fn = jade.compile(tmpl, {
+            pretty: true
+        });
 
         var keyArry = [];
-        for( var key in data.tags ) {
-           keyArry.push( key ); 
+        for (var key in data.tags) {
+            keyArry.push(key);
 
-           //保证顺序
-           data.tags[key] = data.tags[key].sort(function(a, b){ return a.name < b.name}); 
+            //保证顺序
+            data.tags[key] = data.tags[key].sort(function(a, b) {
+                return a.name < b.name;
+            });
         }
 
         //保证顺序
         data.keyArry = keyArry.sort();
 
-		fs.writeFile(DSTFILE, fn(data), 'utf-8', function(err) {
-			if (err) {
-				console.log(err);
-			} else {
-				console.log('cool! u got it!');
-			}
-		})
-	});
+        fs.writeFile(DSTFILE, fn(data), 'utf-8', function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('cool! u got it!');
+            }
+        });
+    });
 }
 
 fs.readdir(DATAFOLDER, function(err, files) {
-	fileCount = files.length;
-	files.forEach(function(v, i) {
-		fs.readFile(path.join(DATAFOLDER, v), 'utf8', function(err, j) {
-			var siteName, o, tags;
-			//siteName = v.replace(/\.json$/, '');
-			o = eval('(' + j.trim() + ')');
+    fileCount = files.length;
+    files.forEach(function(v, i) {
+        fs.readFile(path.join(DATAFOLDER, v), 'utf8', function(err, j) {
+            var siteName, o, tags;
+            //siteName = v.replace(/\.json$/, '');
+            o = eval('(' + j.trim() + ')');
             o.name = v;
 
-			tags = o.tags.split(/,| |，| /);
-			tags.forEach(function(v, i) {
-				if (data.tags[v] === undefined) {
-					data.tags[v] = [];
-				}
+            tags = o.tags.split(/,| |，| /);
+            tags.forEach(function(v, i) {
+                if (data.tags[v] === undefined) {
+                    data.tags[v] = [];
+                }
 
-				data.tags[v].push(o);
-			});
-			if (--fileCount === 0) {
-				parseTmpl();
-			}
-		});
-	})
-})
-
+                data.tags[v].push(o);
+            });
+            if (--fileCount === 0) {
+                parseTmpl();
+            }
+        });
+    });
+});
