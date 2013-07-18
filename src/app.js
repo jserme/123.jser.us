0cdd76ad798f6566f8a9a482a3116bb8db26c28e
lucas <huaxinjiayou@gmail.com>
2013-07-19 00:13:43 +0800

    修改了一些，还是有bug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 (function(window, undefined) {
-    var oLastTagDiv;
-    var oTimer;
+    var bSuggestShow = true;
+    var TIPHEIGHT = 450; // 弹窗高度
+    var oLastTagDiv; // 上一个弹窗
+    var oTimer; // 全局计时对象
 
     $(function() {
         fWindowLoad();
@@ -10,26 +12,35 @@
     function fWindowLoad() {
         $('a.tag').mouseenter(fTagMouseEnter)
             .mouseleave(fTagMouseLeave);
+
+        // 意见反馈弹窗
         fSuggest();
+
+        //百度统计自定义事件数据
+        window._hmt = window._hmt || [];
     }
 
     function fTagMouseEnter() {
+        // 隐藏意见反馈
         fHideTipDiv($('a[data-toggle=feedback]'), '007');
-        
+        bSuggestShow = true;
+
         var oTagDiv = $(this);
         var sDescrition = oTagDiv.data('des');
         var sId = oTagDiv.data('index');
         var nHeight = oTagDiv.height();
         var nTop = oTagDiv.position().top;
 
+        // 隐藏上一个弹窗
         clearTimeout(oTimer);
         if(oLastTagDiv && oLastTagDiv.get(0) != oTagDiv.get(0)) {
             fHideTipDiv(oLastTagDiv, sId);
         }
         oLastTagDiv = oTagDiv;
 
-        var sPos = (nTop - $(document.body).scrollTop()) * 2 > $(window).height() ? 'top' : 'bottom';
-
+        // 上方有完整展示空间才向上展示
+        var sPos = (nTop - $(document.body).scrollTop()) - TIPHEIGHT > 0 ? 'top' : 'bottom'
+        console.log(sPos);
         oTagDiv.popover({
             placement: sPos,
             title: '详情',
@@ -52,6 +63,7 @@
 
     function fTagMouseLeave() {
         var oTagDiv = $(this);
+        clearTimeout(oTimer);
         oTimer = setTimeout(function() {
             var sId = oTagDiv.data('index');
             fHideTipDiv(oTagDiv, sId);
@@ -59,7 +71,9 @@
     }
 
     function fHideTipDiv(oTagDiv, sId) {
-        // 移动后还是会重新加载啊啊啊
+        clearTimeout(oTimer);
+        // DOM位置变换后，iframe还是会重新加载，缓存不了啊啊啊
+        // TODO:fixed it
         try {
             var oTipDiv = oTagDiv.data('popover').tip();
             var oIframe = $('iframe', oTipDiv);
@@ -79,14 +93,13 @@
             trigger: 'click'
         });
 
-        var bShow = true;
         oTagDiv.on('click', function() {
-            if(bShow) {
+            if(bSuggestShow) {
                 $('#temp007').replaceWith(fBuildCommentBox('007'));
             } else {
                 fHideTipDiv(oTagDiv, '007');
             }
-            bShow = !bShow;
+            bSuggestShow = !bSuggestShow;
         });
     }
 
